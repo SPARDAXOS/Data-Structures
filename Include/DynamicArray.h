292a765423f889b9ca0f190ed2a0443582ec49bc
Mohammed Tamim <Alexmerceram@hotmail.com>
2023-11-17 15:56:04 +0100

    Added a bunch of new features to DynamicArray

--- Include/DynamicArray.h
+++ Include/DynamicArray.h
@@ -1,4 +1,4 @@
-
+#include <algorithm>
 
 #ifndef DYNAMIC_ARRAY
 #define DYNAMIC_ARRAY
@@ -7,24 +7,34 @@ template<typename T>
 class DynamicArray final {
 	using Type = T;
 	using Iterator = T*;
+	using Reference = T&;
 
 public:
 	explicit DynamicArray() noexcept = default;
-	explicit DynamicArray(size_t capacity) noexcept { Reallocate(capacity); }
+	explicit DynamicArray(size_t capacity) noexcept { Reserve(capacity); }
+	explicit DynamicArray(size_t capacity, Type element) noexcept { Reserve(capacity); std::fill(Begin(), End(), element); }
 
 	~DynamicArray() {
 		if (m_Size > 0)
 			delete[] m_Iterator;
+		std::cout << "dtor" << std::endl;
 	}
 
 	DynamicArray(const DynamicArray& other) {
-		this->std::copy(other);
+		*this = other;
 	}
 	DynamicArray& operator=(const DynamicArray& other) noexcept {
 		if (this->m_Iterator == other.m_Iterator)
 			return *this;
 		else {
+			if (this->m_Size > 0)
+				Clear();
 
+			Reserve(other.m_Capacity);
+			std::memmove(this->m_Iterator, other.m_Iterator, other.m_Size * sizeof(Type));
+			this->m_Size = other.m_Size;
+
+			return *this;
 		}
 	}
 
@@ -35,32 +45,62 @@ public:
 		if (this->m_Iterator == other.m_Iterator)
 			return *this;
 		else {
+			if (this->m_Size > 0)
+				Clear();
 
+			this->m_Iterator = other.m_Iterator;
+			this->m_Size     = other.m_Size;
+			this->m_Capacity = other.m_Capacity;
+
+			other.m_Iterator = nullptr;
+			other.m_Size	 = 0;
+			other.m_Capacity = 0;
 		}
 	}
 
+	inline Reference operator[](unsigned int index) const noexcept { return m_Iterator[index]; }
+
 public:
-	void Pushback(const Type& element) {
+	void Pushback(const Type element) {
 		if (m_Capacity == 0)
-			Reallocate(1);
+			Reserve(1);
 		else if (m_Size == m_Capacity)
-			Reallocate(m_Capacity * 2);
+			Reserve(m_Capacity * 2);
 
 
 		const Iterator NewElement = new Type(element);
 		std::memmove(&m_Iterator[m_Size], NewElement, sizeof(Type));
 		m_Size++;
 	}
-	void EmplaceBack(const Type& element) {
+	void EmplaceBack(const Reference element) {
 
 	}
 
-public:
-	inline size_t GetSize() const noexcept { return m_Size; }
-	inline size_t GetCapacity() const noexcept { return m_Capacity; }
+	inline Type At(int index) const {
+		if (index >= m_Size || index < 0)
+			throw std::invalid_argument("Index out of bounds");
 
-private:
-	inline void Reallocate(size_t capacity) {
+		return m_Iterator[index];
+	}
+	inline void Clear() {
+		delete[] m_Iterator;
+		m_Size = 0;
+	}
+
+public:
+	inline Iterator Data() const noexcept { return m_Iterator; }
+	inline size_t Size() const noexcept { return m_Size; }
+	inline size_t Capacity() const noexcept { return m_Capacity; }
+	inline bool Empty() const noexcept { return m_Size > 0; }
+	inline Reference Front() const noexcept { return m_Iterator[0]; }
+	inline Reference Back() const noexcept { return m_Iterator[m_Size - 1]; }
+	inline Iterator Begin() const noexcept { return m_Iterator; }
+	inline Iterator End() const noexcept { return &m_Iterator[m_Capacity]; }
+
+	inline void Reserve(size_t capacity) {
+		if (m_Capacity > capacity) //??
+			return;
+
 		m_Capacity = capacity;
 		Iterator NewBuffer = static_cast<Iterator>(malloc(sizeof(Type) * m_Capacity));
 		if (m_Size == 0) {
@@ -72,6 +112,15 @@ private:
 		free(m_Iterator);
 		m_Iterator = NewBuffer;
 	}
+	//PopBack
+	//Insert
+	//Swap (other vec?)`Make sure to test vecs of different template types
+	//Emplace
+	//Emplace back
+	//ShrinkToFit
+	//Resize
+
+private:
 
 	//Use Bitshift to remove and iterate on the array.
 
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -8,6 +8,12 @@
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
 	try {
 
+		//DynamicArray<int> Array(12, 8);
+
+
+
+
+
 		DynamicArray<int> Array;
 		Array.Pushback(1);
 		Array.Pushback(2);
@@ -16,6 +22,20 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
 		Array.Pushback(5);
 		Array.Pushback(6);
 		Array.Pushback(7);
+
+		//DynamicArray<int> Array2;
+		//Array2.Pushback(8);
+		//Array2.Pushback(9);
+		//Array2.Pushback(10);
+		//Array2.Pushback(11);
+		//Array2.Pushback(12);
+		//Array2.Pushback(13);
+		//Array2.Pushback(14);
+
+		//Array = Array2;
+		std::cout << Array.Front() << std::endl;
+		std::cout << Array.Back() << std::endl;
+		//std::cout << Array.At(1) << std::endl;
 
 	}
 	catch (const std::exception& exception) {
